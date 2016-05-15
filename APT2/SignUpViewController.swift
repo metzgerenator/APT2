b42e72c7905c6d272b620e03205830864dc344a6
metzgerenator <metzgerenator@gmail.com>
2016-05-15 19:44:34 -0400

    need to add facebook sdk

---
+++ APT2/SignUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SignUpViewController.swift
+//  APT2
+//
+//  Created by Michael Metzger  on 5/15/16.
+//  Copyright © 2016 Michael Metzger . All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
